df21a58b5420f959b62ea5201803ff64889c6b1b
jwebs22 <jenny.webster@codeimmersives.com>
2023-02-16 10:52:44 -0500

    Added delete route

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -2,6 +2,7 @@
 
 const Blog = require('../models/Blogs');
 
+//Get all blogs
 async function getAllBlogs(req, res) {
 
     //query blogs 
@@ -11,8 +12,9 @@ async function getAllBlogs(req, res) {
     }catch(e){
       console.log(e);
     }
-}
+};
 
+//Create One Blog
 async function createOneBlog(req, res, next) {
     try {
       //parse out fields from POST request
@@ -21,7 +23,6 @@ async function createOneBlog(req, res, next) {
       const author = req.body.author
       const categories = req.body.category
       const year =  req.body.year;
-  
       //pass fields to new Blog model 
       //notice how it's way more organized and does the type checking for us
       const newBlog = new Blog({
@@ -31,10 +32,8 @@ async function createOneBlog(req, res, next) {
           categories,
           year
       });
-  
       //save our new entry to the database 
       const savedData =  await newBlog.save();
-      
       //return the successful request to the user 
       res.json({
           success: true,
@@ -48,9 +47,87 @@ async function createOneBlog(req, res, next) {
         error: e.toString(),
       });
     }
-  }
+  };
+
+
+  //Get one blog
+  async function getOneBlog(req, res, next) {
+
+    try {
+      const oneBlog = await Blog.find({}).limit(1);
+      res.json({blogs: oneBlog });
+    }catch(e){
+      console.log(e);
+    }
+};
+
+
+  // Get one by ID --DONE
+  async function getOneById(req, res, next) {
+
+    try {
+      const oneBlogId = await Blog.find({id: req.params.id});
+      res.json({blogs: oneBlogId });
+    }catch(e){
+      console.log(e);
+    }
+};
+
+//Dominic's version, not working for me. Crashes program after it returns the blog.
+/*
+async function getOneBlog(req, res, next) {
+
+
+    let oneBlogPost;
+
+    try {
+        oneBlogPost = await Blog.findOne({id: req.params.id});
+    } catch (error) {
+        console.log(error);
+    }
+    res.json({
+        sucess: true,
+        oneBlog: oneBlogPost
+    })
+}
+*/
+
+//Delete one
+//Working, but should error check. Getting true even if no id match
+async function deleteOneBlog(req,res){
+    const entryId = req.params.id;
+
+    try {
+        await Blog.deleteOne({id: entryId});
+    } catch (err) {
+        console.log(err);
+        throw err;  
+    }
+
+    res.json({
+        success: true,
+        message: `blog entry id ${entryId} deleted`
+    })
+}
+
+// //Michael's Delete One: //still needs to set in postman and blogs
+// async function deleteOneBlog(req, res, next) {
+//     try {
+//       const oneBlog = await Blog.findOneAndRemove({"id": req.params.id});
+//       res.json({message: "Removed", 
+//         blog: oneBlog});
+//     }
+//     catch (error) {
+//       res.status(500).send(error);
+//     }
+//   };
+
+  
 
 module.exports = {
     getAllBlogs,
-    createOneBlog
+    createOneBlog,
+    getOneBlog,
+    getOneById,
+    deleteOneBlog
 };
